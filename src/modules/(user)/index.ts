e1bb074f415834dd7ee50426a6e6b0211f43aeb2
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-31 16:05:54 +0800

    feat: get user by id

--- src/modules/(user)/index.ts
+++ src/modules/(user)/index.ts
@@ -1,5 +1,7 @@
 import { db } from "@/db/connection";
+import { users } from "@/db/schema";
 import { authMiddleware } from "@/modules/(auth)/middleware";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
 export const usersModule = new Hono();
@@ -14,6 +16,38 @@ usersModule.get("/", async (c) => {
     });
 
     return c.json(users);
+});
+
+usersModule.get("/:id", async (c) => {
+    const id = c.req.param("id");
+    if (!id) {
+        return c.json(
+            {
+                success: false,
+                message: "ID is required",
+            },
+            400,
+        );
+    }
+
+    const user = await db.query.users.findFirst({
+        where: eq(users.id, id),
+        columns: {
+            password: false,
+        },
+    });
+
+    if (!user) {
+        return c.json(
+            {
+                success: false,
+                message: "User not found",
+            },
+            404,
+        );
+    }
+
+    return c.json(user);
 });
 
 export default usersModule;
